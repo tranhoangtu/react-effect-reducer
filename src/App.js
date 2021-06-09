b2987789712761903a487b0dd376272665f0c0a8
Tran Tu <tranhoangtu12091994@gmail.com>
2021-06-09 15:20:30 +0700

    add useeffect import

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 
 import Login from "./components/Login/Login";
 import Home from "./components/Home/Home";
