48800f02f0ee1722041095c576ec147590d7b4c6
Tran Tu <tranhoangtu12091994@gmail.com>
2021-06-09 15:55:18 +0700

    Add clean up function for use effect

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -12,9 +12,16 @@ const Login = (props) => {
   const [formIsValid, setFormIsValid] = useState(false);
 
   useEffect(() => {
-    setFormIsValid(
-      enteredEmail.includes("@") && enteredPassword.trim().length > 6
-    );
+    const identifier = setTimeout(() => {
+      console.log("Side effect");
+      setFormIsValid(
+        enteredEmail.includes("@") && enteredPassword.trim().length > 6
+      );
+    }, 500);
+    return () => {
+      console.log("Clean up");
+      clearTimeout(identifier);
+    };
   }, [enteredEmail, enteredPassword]);
 
   const emailChangeHandler = (event) => {
